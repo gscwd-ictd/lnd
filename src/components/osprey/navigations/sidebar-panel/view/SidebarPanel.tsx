dc5f642413a525a8c541395fecb775c5532fe96c
ericsison <ericsison.dev@gmail.com>
2023-05-15 08:32:23 +0800

    modified sidebar panel footer

--- src/components/osprey/navigations/sidebar-panel/view/SidebarPanel.tsx
+++ src/components/osprey/navigations/sidebar-panel/view/SidebarPanel.tsx
@@ -25,8 +25,11 @@ export const SidebarPanel: FunctionComponent = () => {
           </ul>
         </main>
 
-        {/* <footer className="h-14">footer</footer> */}
-        <SidebarPanelFooter />
+        <SidebarPanelFooter
+          photoUrl="https://images.unsplash.com/photo-1568602471122-7832951cc4c5?ixlib=rb-4.0.3&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=facearea&facepad=2&w=300&h=300&q=80"
+          name="John Doe"
+          email="johndoe@gmail.com"
+        />
       </div>
     </div>
   );
