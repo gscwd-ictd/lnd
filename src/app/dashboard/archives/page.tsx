815fb12f1c5201a60e2c76560ffcfdf8422a4863
ericsison <ericsison.dev@gmail.com>
2023-05-23 11:22:17 +0800

    updated some pages to test components

--- src/app/dashboard/archives/page.tsx
+++ src/app/dashboard/archives/page.tsx
@@ -1,3 +1,13 @@
+"use client";
+
+import { ColumnSearchInput } from "@lms/components/osprey/ui/data-table-test/view/ColumnSearchInput";
+
 export default function DashboardArchives() {
-  return <div>Archives</div>;
+  return (
+    <div className="flex h-full w-full items-center justify-center">
+      <div className="h-10 w-96">
+        <ColumnSearchInput onSearch={(e) => console.log(e)} debounce={500} className="h-full" placeholder="Search" />
+      </div>
+    </div>
+  );
 }
