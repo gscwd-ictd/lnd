f8fcd6d9b10bd8784d7ae8ac8e485347bf7b76a1
jayrln <jaynosotros@gmail.com>
2023-05-23 14:10:40 +0700

    Testing page for my newcomponents

--- src/app/settings/training-type/page.tsx
+++ src/app/settings/training-type/page.tsx
@@ -55,14 +55,20 @@
 
 import { Button } from "@lms/components/osprey/ui/button/view/Button";
 import { Provider, Viewport } from "@radix-ui/react-toast";
-import { Toast, ToastProvider, ToastViewport } from "@lms/components/osprey/ui/overlays/toast/view/toast";
 import { useState } from "react";
 import { useToastStore } from "@lms/components/osprey/ui/overlays/toast/utils/store";
-import { Alert } from "@lms/components/osprey/ui/overlays/alert/view/alert";
+import { TabContentWrap, Tabs, TabsContent, TabsList, TabsTrigger } from "@lms/components/osprey/ui/tabs/view/Tabs";
+import Image from "next/image";
+import TestPic from "../../../lib/img/test.jpg";
+// import * as Tabs from "@radix-ui/react-tabs";
+import { Toast, ToastProvider, ToastViewport } from "@lms/components/osprey/ui/overlays/toast/view/Toast";
+import { Alert } from "@lms/components/osprey/ui/overlays/alert/view/Alert";
 
 export default function TrainingTypeModal() {
   // const [open, setOpen] = useState(false);
   const { setOpen } = useToastStore();
+  const [orientation, setOrientation] = useState<"horizontal" | "vertical" | undefined>("vertical");
+  const [danger, setDanger] = useState(false);
 
   return (
     <>
@@ -74,161 +80,134 @@ export default function TrainingTypeModal() {
         >
           Sample Toast
         </Button>
-        <Toast
-          // open={open}
-          // onOpenChange={setOpen}
-          title="Pull Request Review"
-          content="Someone requested your review on"
-        />
+        <Toast title="Pull Request Review" content="Someone requested your review on" />
         <ToastViewport className="ToastViewport" />
       </ToastProvider>
 
       <div className="mt-16 p-4">
-        <Alert title="Title" content="Content" color="danger"></Alert>
         <>Success</>
-        <div className="bg-teal-100  text-teal-700 px-4 py-3 rounded relative shadow-md" role="alert">
-          <div className="flex">
-            <div className="py-1">
+        <Alert title="Success">
+          Lorem, ipsum dolor sit amet consectetur adipisicing elit. Quod maxime provident tempore quisquam! Facilis
+          deleniti mollitia possimus vel repudiandae sit eaque necessitatibus voluptas. Reiciendis expedita, delectus a
+          modi maxime nobis?
+        </Alert>
+        <>Error</>
+        <Button onClick={() => setDanger(true)}>Set Alert</Button>
+        {danger && (
+          <Alert title="Title" color="danger">
+            Lorem, ipsum dolor sit amet consectetur adipisicing elit. Quod maxime provident tempore quisquam! Facilis
+            deleniti mollitia possimus vel repudiandae sit eaque necessitatibus voluptas. Reiciendis expedita, delectus
+            a modi maxime nobis?
+          </Alert>
+        )}
+        <>Info</>
+        <Alert title="Title" color="info">
+          Lorem, ipsum dolor sit amet consectetur adipisicing elit. Quod maxime provident tempore quisquam! Facilis
+          deleniti mollitia possimus vel repudiandae sit eaque necessitatibus voluptas. Reiciendis expedita, delectus a
+          modi maxime nobis?
+        </Alert>
+        <>Warning</>
+        <Alert title="Title" color="warning">
+          Lorem, ipsum dolor sit amet consectetur adipisicing elit. Quod maxime provident tempore quisquam! Facilis
+          deleniti mollitia possimus vel repudiandae sit eaque necessitatibus voluptas. Reiciendis expedita, delectus a
+          modi maxime nobis?
+        </Alert>
+      </div>
+
+      <div className="mt-5 px-3">
+        <Tabs defaultValue="tab1" orientation="horizontal">
+          <TabsList>
+            <TabsTrigger value="tab1">One</TabsTrigger>
+            <TabsTrigger value="tab2">Two</TabsTrigger>
+            <TabsTrigger value="tab3">Three</TabsTrigger>
+          </TabsList>
+          <TabContentWrap>
+            <TabsContent value="tab1">
+              Lorem, ipsum dolor sit amet consectetur adipisicing elit. Quod maxime provident tempore quisquam! Facilis
+              deleniti mollitia possimus vel repudiandae sit eaque necessitatibus voluptas. Reiciendis expedita,
+              delectus a modi maxime nobis?
+            </TabsContent>
+            <TabsContent value="tab2">2</TabsContent>
+            <TabsContent value="tab3">3</TabsContent>
+          </TabContentWrap>
+        </Tabs>
+      </div>
+
+      <div className="mt-5 px-3">
+        <div className="flex flex-row gap-4">
+          {/* <Card></Card> */}
+          {/* Card (default) */}
+          <div className="block max-w-sm p-6 bg-white border border-gray-200 rounded-lg shadow hover:bg-gray-100">
+            <h5 className="text-2xl font-bold tracking-tight">Header</h5>
+            <h6 className="mb-2">Subheader</h6>
+            <p className="text-gray-700 ">
+              Lorem ipsum dolor sit amet consectetur adipisicing elit. Enim sed in, reprehenderit ipsam odio temporibus
+              eaque, non soluta inventore, numquam unde repellat? Laborum, pariatur et ipsam vel sapiente eos amet?
+            </p>
+          </div>
+          {/* Card with button/link */}
+          <div className="max-w-sm p-6 bg-white border border-gray-200 rounded-lg shadow">
+            <h5 className="text-2xl font-bold tracking-tight text-gray-900 ">Header</h5>
+            <h6 className="mb-2">Subheader</h6>
+            <p className="mb-3 font-normal text-gray-700">
+              Here are the biggest enterprise technology acquisitions of 2021 so far, in reverse chronological order.
+            </p>
+            <a
+              href="#"
+              className="inline-flex items-center px-3 py-2 text-sm font-medium text-center text-white bg-blue-700 rounded-lg hover:bg-blue-800 focus:ring-4 focus:outline-none focus:ring-blue-300 dark:bg-blue-600 dark:hover:bg-blue-700 dark:focus:ring-blue-800"
+            >
+              Read more
               <svg
-                fill="#000000"
-                viewBox="0 0 24 24"
-                className="fill-current h-8 w-8 text-teal-500 mr-4"
+                aria-hidden="true"
+                className="w-4 h-4 ml-2 -mr-1"
+                fill="currentColor"
+                viewBox="0 0 20 20"
                 xmlns="http://www.w3.org/2000/svg"
               >
                 <path
                   fillRule="evenodd"
-                  d="M12,2 C17.5228475,2 22,6.4771525 22,12 C22,17.5228475 17.5228475,22 12,22 C6.4771525,22 2,17.5228475 2,12 C2,6.4771525 6.4771525,2 12,2 Z M12,4 C7.581722,4 4,7.581722 4,12 C4,16.418278 7.581722,20 12,20 C16.418278,20 20,16.418278 20,12 C20,7.581722 16.418278,4 12,4 Z M15.2928932,8.29289322 L10,13.5857864 L8.70710678,12.2928932 C8.31658249,11.9023689 7.68341751,11.9023689 7.29289322,12.2928932 C6.90236893,12.6834175 6.90236893,13.3165825 7.29289322,13.7071068 L9.29289322,15.7071068 C9.68341751,16.0976311 10.3165825,16.0976311 10.7071068,15.7071068 L16.7071068,9.70710678 C17.0976311,9.31658249 17.0976311,8.68341751 16.7071068,8.29289322 C16.3165825,7.90236893 15.6834175,7.90236893 15.2928932,8.29289322 Z"
+                  d="M10.293 3.293a1 1 0 011.414 0l6 6a1 1 0 010 1.414l-6 6a1 1 0 01-1.414-1.414L14.586 11H3a1 1 0 110-2h11.586l-4.293-4.293a1 1 0 010-1.414z"
+                  clipRule="evenodd"
                 ></path>
               </svg>
-            </div>
-            <div>
-              <p className="font-bold">Success</p>
-              <p className="text-sm">
-                Lorem ipsum dolor, sit amet consectetur adipisicing elit. Facilis cumque magni dignissimos illum,
-                possimus consectetur eius odio obcaecati incidunt quas mollitia repudiandae laudantium nobis
-                reprehenderit ullam iusto hic fuga nisi?
-              </p>
-            </div>
-            <div className="absolute top-0 bottom-0 right-0 px-4 py-3">
-              <svg
-                className="fill-current h-6 w-6 text-teal-500 "
-                role="button"
-                xmlns="http://www.w3.org/2000/svg"
-                viewBox="0 0 20 20"
-              >
-                <title>Close</title>
-                <path d="M14.348 14.849a1.2 1.2 0 0 1-1.697 0L10 11.819l-2.651 3.029a1.2 1.2 0 1 1-1.697-1.697l2.758-3.15-2.759-3.152a1.2 1.2 0 1 1 1.697-1.697L10 8.183l2.651-3.031a1.2 1.2 0 1 1 1.697 1.697l-2.758 3.152 2.758 3.15a1.2 1.2 0 0 1 0 1.698z" />
-              </svg>
-            </div>
+            </a>
           </div>
+          {/* Card with image */}
         </div>
-        <>Error</>
-        <div className="bg-red-100  text-red-700 px-4 py-3 rounded relative shadow-md" role="alert">
-          <div className="flex">
-            <div className="py-1">
+      </div>
+      <div className="mt-5 px-3 pb-3">
+        <div className="max-w-sm bg-white border border-gray-200 rounded-lg shadow ">
+          <div>
+            <Image className="rounded-t-lg" src={TestPic} width={500} height={300} alt="Picture of the author" />
+          </div>
+          <div className="p-5">
+            <a href="#">
+              <h5 className="mb-2 text-2xl font-bold tracking-tight text-gray-900 ">
+                Noteworthy technology acquisitions 2021
+              </h5>
+            </a>
+            <p className="mb-3 font-normal text-gray-700 ">
+              Here are the biggest enterprise technology acquisitions of 2021 so far, in reverse chronological order.
+            </p>
+            <a
+              href="#"
+              className="inline-flex items-center px-3 py-2 text-sm font-medium text-center text-white bg-blue-700 rounded-lg hover:bg-blue-800 focus:ring-4 focus:outline-none focus:ring-blue-300 dark:bg-blue-600 dark:hover:bg-blue-700 dark:focus:ring-blue-800"
+            >
+              Read more
               <svg
-                viewBox="0 0 512 512"
+                aria-hidden="true"
+                className="w-4 h-4 ml-2 -mr-1"
+                fill="currentColor"
+                viewBox="0 0 20 20"
                 xmlns="http://www.w3.org/2000/svg"
-                className="fill-current h-8 w-8 text-red-500 mr-4"
               >
-                <title>error</title>
                 <path
-                  d="M213.333333,3.55271368e-14 C331.136,3.55271368e-14 426.666667,95.5306667 426.666667,213.333333 C426.666667,331.136 331.136,426.666667 213.333333,426.666667 C95.5306667,426.666667 3.55271368e-14,331.136 3.55271368e-14,213.333333 C3.55271368e-14,95.5306667 95.5306667,3.55271368e-14 213.333333,3.55271368e-14 Z M213.333333,42.6666667 C119.232,42.6666667 42.6666667,119.232 42.6666667,213.333333 C42.6666667,307.434667 119.232,384 213.333333,384 C307.434667,384 384,307.434667 384,213.333333 C384,119.232 307.434667,42.6666667 213.333333,42.6666667 Z M262.250667,134.250667 L292.416,164.416 L243.498667,213.333333 L292.416,262.250667 L262.250667,292.416 L213.333333,243.498667 L164.416,292.416 L134.250667,262.250667 L183.168,213.333333 L134.250667,164.416 L164.416,134.250667 L213.333333,183.168 L262.250667,134.250667 Z"
-                  id="error"
+                  fillRule="evenodd"
+                  d="M10.293 3.293a1 1 0 011.414 0l6 6a1 1 0 010 1.414l-6 6a1 1 0 01-1.414-1.414L14.586 11H3a1 1 0 110-2h11.586l-4.293-4.293a1 1 0 010-1.414z"
+                  clipRule="evenodd"
                 ></path>
               </svg>
-            </div>
-            <div>
-              <p className="font-bold">Error</p>
-              <p className="text-sm">
-                Lorem ipsum dolor, sit amet consectetur adipisicing elit. Facilis cumque magni dignissimos illum,
-                possimus consectetur eius odio obcaecati incidunt quas mollitia repudiandae laudantium nobis
-                reprehenderit ullam iusto hic fuga nisi?
-              </p>
-            </div>
-            <div className="absolute top-0 bottom-0 right-0 px-4 py-3">
-              <svg
-                className="fill-current h-6 w-6 text-red-500 "
-                role="button"
-                xmlns="http://www.w3.org/2000/svg"
-                viewBox="0 0 20 20"
-              >
-                <title>Close</title>
-                <path d="M14.348 14.849a1.2 1.2 0 0 1-1.697 0L10 11.819l-2.651 3.029a1.2 1.2 0 1 1-1.697-1.697l2.758-3.15-2.759-3.152a1.2 1.2 0 1 1 1.697-1.697L10 8.183l2.651-3.031a1.2 1.2 0 1 1 1.697 1.697l-2.758 3.152 2.758 3.15a1.2 1.2 0 0 1 0 1.698z" />
-              </svg>
-            </div>
-          </div>
-        </div>
-        <>Info</>
-        <div className="bg-blue-100  text-blue-700 px-4 py-3 rounded relative shadow-md" role="alert">
-          <div className="flex">
-            <div className="py-1">
-              <svg
-                viewBox="0 0 48 48"
-                xmlns="http://www.w3.org/2000/svg"
-                className="fill-current h-8 w-8 text-blue-500 mr-4"
-                fill="#000000"
-              >
-                <title>information-circle</title>
-                <path d="M24,2A22,22,0,1,0,46,24,21.9,21.9,0,0,0,24,2Zm0,40A18,18,0,1,1,42,24,18.1,18.1,0,0,1,24,42Z"></path>{" "}
-                <path d="M24,20a2,2,0,0,0-2,2V34a2,2,0,0,0,4,0V22A2,2,0,0,0,24,20Z"></path>
-                <circle cx="24" cy="14" r="2"></circle>
-              </svg>
-            </div>
-            <div>
-              <p className="font-bold">Information</p>
-              <p className="text-sm">
-                Lorem ipsum dolor, sit amet consectetur adipisicing elit. Facilis cumque magni dignissimos illum,
-                possimus consectetur eius odio obcaecati incidunt quas mollitia repudiandae laudantium nobis
-                reprehenderit ullam iusto hic fuga nisi?
-              </p>
-            </div>
-            <div className="absolute top-0 bottom-0 right-0 px-4 py-3">
-              <svg
-                className="fill-current h-6 w-6 text-blue-500 "
-                role="button"
-                xmlns="http://www.w3.org/2000/svg"
-                viewBox="0 0 20 20"
-              >
-                <title>Close</title>
-                <path d="M14.348 14.849a1.2 1.2 0 0 1-1.697 0L10 11.819l-2.651 3.029a1.2 1.2 0 1 1-1.697-1.697l2.758-3.15-2.759-3.152a1.2 1.2 0 1 1 1.697-1.697L10 8.183l2.651-3.031a1.2 1.2 0 1 1 1.697 1.697l-2.758 3.152 2.758 3.15a1.2 1.2 0 0 1 0 1.698z" />
-              </svg>
-            </div>
-          </div>
-        </div>
-        <>Warning</>
-        <div className="bg-orange-100  text-orange-700 px-4 py-3 rounded relative shadow-md" role="alert">
-          <div className="flex">
-            <div className="py-1">
-              <svg viewBox="0 0 24 24" fill="none" xmlns="http://www.w3.org/2000/svg" className="h-8 w-8 mr-4">
-                <path
-                  d="M12 15H12.01M12 12V9M4.98207 19H19.0179C20.5615 19 21.5233 17.3256 20.7455 15.9923L13.7276 3.96153C12.9558 2.63852 11.0442 2.63852 10.2724 3.96153L3.25452 15.9923C2.47675 17.3256 3.43849 19 4.98207 19Z"
-                  stroke="#f97316"
-                  strokeWidth="2"
-                  strokeLinecap="round"
-                  strokeLinejoin="round"
-                ></path>
-              </svg>
-            </div>
-            <div>
-              <p className="font-bold">Information</p>
-              <p className="text-sm">
-                Lorem ipsum dolor, sit amet consectetur adipisicing elit. Facilis cumque magni dignissimos illum,
-                possimus consectetur eius odio obcaecati incidunt quas mollitia repudiandae laudantium nobis
-                reprehenderit ullam iusto hic fuga nisi?
-              </p>
-            </div>
-            <div className="absolute top-0 bottom-0 right-0 px-4 py-3">
-              <svg
-                className="fill-current h-6 w-6 text-orange-500 "
-                role="button"
-                xmlns="http://www.w3.org/2000/svg"
-                viewBox="0 0 20 20"
-              >
-                <title>Close</title>
-                <path d="M14.348 14.849a1.2 1.2 0 0 1-1.697 0L10 11.819l-2.651 3.029a1.2 1.2 0 1 1-1.697-1.697l2.758-3.15-2.759-3.152a1.2 1.2 0 1 1 1.697-1.697L10 8.183l2.651-3.031a1.2 1.2 0 1 1 1.697 1.697l-2.758 3.152 2.758 3.15a1.2 1.2 0 0 1 0 1.698z" />
-              </svg>
-            </div>
+            </a>
           </div>
         </div>
       </div>
