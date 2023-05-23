239859b27342bdbef036158f2b884d37ec2d2c84
jayrln <jaynosotros@gmail.com>
2023-05-23 14:13:03 +0700

    Tabs component. Partial styling

---
+++ src/components/osprey/ui/tabs/utils/styles.ts
@@ -0,0 +1,6 @@
+export const styles = {
+  root: "flex flex-col w-full bg-white",
+  list: "flex-1 border-b",
+  trigger: "flex-1 px px-4 py-2 hover:text-midnight-500 focus:border-b-2 data-[state=active]:border-b-2",
+  content: "px-3 py-3",
+};
