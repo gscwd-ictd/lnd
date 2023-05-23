75ec7e48e79c70a8432e14dc0fee880feceaecb1
jayrln <jaynosotros@gmail.com>
2023-05-23 14:09:14 +0700

    fixed some alert ui

--- src/components/osprey/ui/overlays/alert/view/alert.tsx
+++ src/components/osprey/ui/overlays/alert/view/alert.tsx
@@ -3,7 +3,7 @@ import { styles } from "../utils/style";
 import { AlertProps } from "../utils/props";
 import { AnimatePresence, motion } from "framer-motion";
 
-export const Alert: FunctionComponent<AlertProps> = ({ title, content, color = "success" }) => {
+export const Alert: FunctionComponent<AlertProps> = ({ title, children, color = "success" }) => {
   const [close, setClose] = useState(true);
 
   const startIcon = (color: String) => {
@@ -17,7 +17,7 @@ export const Alert: FunctionComponent<AlertProps> = ({ title, content, color = "
             xmlns="http://www.w3.org/2000/svg"
           >
             <path
-              fill-rule="evenodd"
+              fillRule="evenodd"
               d="M12,2 C17.5228475,2 22,6.4771525 22,12 C22,17.5228475 17.5228475,22 12,22 C6.4771525,22 2,17.5228475 2,12 C2,6.4771525 6.4771525,2 12,2 Z M12,4 C7.581722,4 4,7.581722 4,12 C4,16.418278 7.581722,20 12,20 C16.418278,20 20,16.418278 20,12 C20,7.581722 16.418278,4 12,4 Z M15.2928932,8.29289322 L10,13.5857864 L8.70710678,12.2928932 C8.31658249,11.9023689 7.68341751,11.9023689 7.29289322,12.2928932 C6.90236893,12.6834175 6.90236893,13.3165825 7.29289322,13.7071068 L9.29289322,15.7071068 C9.68341751,16.0976311 10.3165825,16.0976311 10.7071068,15.7071068 L16.7071068,9.70710678 C17.0976311,9.31658249 17.0976311,8.68341751 16.7071068,8.29289322 C16.3165825,7.90236893 15.6834175,7.90236893 15.2928932,8.29289322 Z"
             ></path>
           </svg>
@@ -56,9 +56,9 @@ export const Alert: FunctionComponent<AlertProps> = ({ title, content, color = "
             <path
               d="M12 15H12.01M12 12V9M4.98207 19H19.0179C20.5615 19 21.5233 17.3256 20.7455 15.9923L13.7276 3.96153C12.9558 2.63852 11.0442 2.63852 10.2724 3.96153L3.25452 15.9923C2.47675 17.3256 3.43849 19 4.98207 19Z"
               stroke="#f97316"
-              stroke-width="2"
-              stroke-linecap="round"
-              stroke-linejoin="round"
+              strokeWidth="2"
+              strokeLinecap="round"
+              strokeLinejoin="round"
             ></path>
           </svg>
         );
@@ -74,6 +74,9 @@ export const Alert: FunctionComponent<AlertProps> = ({ title, content, color = "
             role="button"
             xmlns="http://www.w3.org/2000/svg"
             viewBox="0 0 20 20"
+            onClick={() => {
+              setClose(false);
+            }}
           >
             <title>Close</title>
             <path d="M14.348 14.849a1.2 1.2 0 0 1-1.697 0L10 11.819l-2.651 3.029a1.2 1.2 0 1 1-1.697-1.697l2.758-3.15-2.759-3.152a1.2 1.2 0 1 1 1.697-1.697L10 8.183l2.651-3.031a1.2 1.2 0 1 1 1.697 1.697l-2.758 3.152 2.758 3.15a1.2 1.2 0 0 1 0 1.698z" />
@@ -97,27 +100,31 @@ export const Alert: FunctionComponent<AlertProps> = ({ title, content, color = "
       case "info":
         return (
           <svg
-            viewBox="0 0 48 48"
+            className="fill-current h-6 w-6 text-blue-500"
+            role="button"
             xmlns="http://www.w3.org/2000/svg"
-            className="fill-current h-8 w-8 text-blue-500 mr-4"
-            fill="#000000"
+            viewBox="0 0 20 20"
+            onClick={() => {
+              setClose(false);
+            }}
           >
-            <title>information-circle</title>
-            <path d="M24,2A22,22,0,1,0,46,24,21.9,21.9,0,0,0,24,2Zm0,40A18,18,0,1,1,42,24,18.1,18.1,0,0,1,24,42Z"></path>{" "}
-            <path d="M24,20a2,2,0,0,0-2,2V34a2,2,0,0,0,4,0V22A2,2,0,0,0,24,20Z"></path>
-            <circle cx="24" cy="14" r="2"></circle>
+            <title>Close</title>
+            <path d="M14.348 14.849a1.2 1.2 0 0 1-1.697 0L10 11.819l-2.651 3.029a1.2 1.2 0 1 1-1.697-1.697l2.758-3.15-2.759-3.152a1.2 1.2 0 1 1 1.697-1.697L10 8.183l2.651-3.031a1.2 1.2 0 1 1 1.697 1.697l-2.758 3.152 2.758 3.15a1.2 1.2 0 0 1 0 1.698z" />
           </svg>
         );
       case "warning":
         return (
-          <svg viewBox="0 0 24 24" fill="none" xmlns="http://www.w3.org/2000/svg" className="h-8 w-8 mr-4">
-            <path
-              d="M12 15H12.01M12 12V9M4.98207 19H19.0179C20.5615 19 21.5233 17.3256 20.7455 15.9923L13.7276 3.96153C12.9558 2.63852 11.0442 2.63852 10.2724 3.96153L3.25452 15.9923C2.47675 17.3256 3.43849 19 4.98207 19Z"
-              stroke="#f97316"
-              strokeWidth="2"
-              strokeLinecap="round"
-              strokeLinejoin="round"
-            ></path>
+          <svg
+            className="fill-current h-6 w-6 text-orange-500"
+            role="button"
+            xmlns="http://www.w3.org/2000/svg"
+            viewBox="0 0 20 20"
+            onClick={() => {
+              setClose(false);
+            }}
+          >
+            <title>Close</title>
+            <path d="M14.348 14.849a1.2 1.2 0 0 1-1.697 0L10 11.819l-2.651 3.029a1.2 1.2 0 1 1-1.697-1.697l2.758-3.15-2.759-3.152a1.2 1.2 0 1 1 1.697-1.697L10 8.183l2.651-3.031a1.2 1.2 0 1 1 1.697 1.697l-2.758 3.152 2.758 3.15a1.2 1.2 0 0 1 0 1.698z" />
           </svg>
         );
     }
@@ -137,7 +144,8 @@ export const Alert: FunctionComponent<AlertProps> = ({ title, content, color = "
             <div className="py-1">{startIcon(color)}</div>
             <div>
               <p className="font-bold">{title}</p>
-              <p className="text-sm">{content}</p>
+              {/* <p className="text-sm">{content}</p> */}
+              {children}
             </div>
             <div className="absolute top-0 bottom-0 right-0 px-4 py-3">{closeIcon(color)}</div>
           </div>
