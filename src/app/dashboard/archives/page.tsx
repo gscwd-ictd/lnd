0942d6aff5cf7d174e0aa4a435021f9eab5f8e41
ericsison <ericsison.dev@gmail.com>
2023-05-23 11:31:35 +0800

    fixed unresolved import error

--- src/app/dashboard/archives/page.tsx
+++ src/app/dashboard/archives/page.tsx
@@ -1,13 +1,9 @@
 "use client";
 
-import { ColumnSearchInput } from "@lms/components/osprey/ui/data-table-test/view/ColumnSearchInput";
-
 export default function DashboardArchives() {
   return (
     <div className="flex h-full w-full items-center justify-center">
-      <div className="h-10 w-96">
-        <ColumnSearchInput onSearch={(e) => console.log(e)} debounce={500} className="h-full" placeholder="Search" />
-      </div>
+      <div className="h-10 w-96">Archives</div>
     </div>
   );
 }
