5bba316456cdf3358a3eb37c1e3b0d40b802d1b2
jayrln <jaynosotros@gmail.com>
2023-05-26 08:55:59 +0700

    fixed tab styling

--- src/components/osprey/ui/tabs/utils/styles.ts
+++ src/components/osprey/ui/tabs/utils/styles.ts
@@ -1,6 +1,7 @@
 export const styles = {
   root: "flex flex-col w-full bg-white",
-  list: "flex-1 border-b",
-  trigger: "flex-1 px px-4 py-2 hover:text-midnight-500 focus:border-b-2 data-[state=active]:border-b-2",
+  // list: "flex-1 ",
+  // focus:border-b-2
+  trigger: "px px-4 py-2 hover:bg-slate-100  data-[state=active]:border-b-2",
   content: "px-3 py-3",
 };
