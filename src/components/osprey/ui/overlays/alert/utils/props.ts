c149abf45faaf4659ee852dcae0d5d3aaaa2234d
jayrln <jaynosotros@gmail.com>
2023-05-26 15:17:07 +0700

    deleted alert component in overlay file path

--- src/components/osprey/ui/overlays/alert/utils/props.ts
+++
@@ -1,8 +0,0 @@
-import { ReactNode } from "react";
-
-export type AlertProps = {
-  title: string;
-  // content: string;
-  color?: "success" | "danger" | "info" | "warning";
-  children: ReactNode | ReactNode[];
-};
